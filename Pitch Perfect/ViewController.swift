f46ee41327283e6aa98c539b059bd53949d579a0
Shyam <shyam.pgmr@gmail.com>
2017-03-07 09:25:20 +0530

    Feat: Add IBOutlet, IBAction for startRecord, stopRecord & icons into ImageAsset

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Action
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Recording stopped"
+    }
+    
+    
 }
 
